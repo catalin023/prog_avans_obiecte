6bb1277b2c5622f47a434487919fb8f571279f08
Rapca Catalin <rapceacatalinc2gmail.com>
2024-03-26 14:23:30 +0200

    lab5_StringRegex

---
+++ src/lab5/StringRegex/Applcation.java
@@ -0,0 +1,19 @@
+package lab5.StringRegex;
+
+public class Applcation {
+    public static void main(String[] args) {
+
+        System.out.println("a".matches("[abc]"));
+        System.out.println("k".matches("[^abc]"));
+        System.out.println("z".matches("[a-z]"));
+        System.out.println("B".matches("[a-zA-Z]"));
+        System.out.println("jK".matches("[a-z][A-Z]"));
+        System.out.println("abacbac".matches("[abc]+"));
+        System.out.println("".matches("[abc]*"));
+        System.out.println("abcab".matches("[abc]{5}"));
+        System.out.println("abacbabc".matches("[abc]{5,}"));
+        System.out.println("cbabbbca".matches("[abc]{5,10}"));
+        System.out.println("a".matches("[abc]"));
+
+    }
+}
