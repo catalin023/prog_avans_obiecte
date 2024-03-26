20c06d037b2e9179f02dab1f369a28d96da8979e
Rapca Catalin <rapceacatalinc2gmail.com>
2024-03-26 14:46:12 +0200

    lab5_Ex2

--- src/lab5/StringEx2/Application.java
+++ src/lab5/StringEx2/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         String text = new String("Odata creat un sir de caractere cu continutul sau nu mai poate fi modificat.");
         System.out.println(text.length());
-        System.out.println(text.matches("[a-zA-Z]*"));
+        System.out.println(text.matches("[a-zA-Z .]*"));
         String[] cuvinte = text.split(" ");
 
         if(cuvinte.length%2==0){
